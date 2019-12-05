59744d4148e8c3fcf93623a0ee127ef69ee3ff12
Leo Gray <leograyc@gmail.com>
2019-12-05 15:32:20 -0500

    added fft functions

--- C/VisualSIgnals.m
+++ C/VisualSIgnals.m
@@ -1,3 +1,15 @@
 M = csvread( "modulated_binary.csv" );
 t = M(:,1);
 y = M(:,2);
+figure(1)
+plot(t(1:300),y(1:300))
+
+fs = 100000;
+n = length(y);
+ftY = fft(y);
+ftY0 = fftshift(ftY);         % shift y values
+ftY0 = abs(ftY0).^2/ n ;    % 0-centered power
+
+figure(2)
+f0 = (-n/2:n/2-1)*(fs/n); % 0-centered frequency range
+plot(f0,ftY0,'b')
